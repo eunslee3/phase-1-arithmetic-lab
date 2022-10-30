9e17c790682013a82c89dad502196c3e39acb348
Samuel <lee.eunseong7856@gmail.com>
2022-10-30 17:18:01 -0500

    commit tree 46c00738403c464b6f772bba0d712d2ac1cbdd75

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10 + 1);
+
+const num3 = 44;
+const num4 = 10;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 5, 20);
